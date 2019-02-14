03401603165a4d2d81386ba1dc2b8c0c7aba5537
Thomas Motzkus <akimbobigmac@gmail.com>
2019-02-14 09:34:49 +0100

    forskellige operatorer funktion

--- Git/Git/Program.cs
+++ Git/Git/Program.cs
@@ -10,6 +10,15 @@ namespace Git
     {
         static void Main(string[] args)
         {
+
+            double[] bla = Forskellige(4, 6);
+
+            foreach (double tal in bla)
+            {
+                Console.WriteLine(tal);
+            }
+
+            
         }
 
         static int Sum(int a, int b)
@@ -24,6 +33,20 @@ namespace Git
             double resultat = a / b;
 
             return resultat;
+        }
+
+        static double Gange(int a, int b, int c)
+        {
+            double resultat = a * b * c;
+
+            return resultat;
+        }
+
+        static double[] Forskellige(int a, int b)
+        {
+            double[] alle = { a + b , a - b, a * b, a / b};
+
+            return alle;
         }
     }
 }
